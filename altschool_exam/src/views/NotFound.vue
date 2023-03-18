0233d1f5ab06d167e1e159f3baccc74ed4ce7fd2
einsteinnnamah <einsteinnnamah@gmail.com>
2023-03-18 21:20:55 +0100

    improved the Notfound

--- altschool_exam/src/views/NotFound.vue
+++ altschool_exam/src/views/NotFound.vue
@@ -1,3 +1,26 @@
 <template>
-  <div>404 page</div>
+  <div class="bg-background_img h-screen w-screen text-white">
+    <div class="flex items-center justify-center flex-col">
+      <img :src="require('../assets/sad-face-emoji.gif')" />
+      <h1 class="font-bold text-[64px]">404 Page</h1>
+      <p class="text-[20px]">This page does not exist</p>
+      <button
+        class="text-white mt-[20px] bg-gradient-to-t from-[#5E3EEB] to-[#FFBAD5] px-[14px] py-[10px] rounded-full"
+        @click="goBack"
+      >
+        Go back to main page
+      </button>
+    </div>
+  </div>
 </template>
+
+<script>
+export default {
+  name: "NotFOund",
+  methods: {
+    goBack() {
+      this.$router.push("/");
+    },
+  },
+};
+</script>
